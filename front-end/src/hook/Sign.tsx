08e0b650d2a9bca3f08198dd174789892fbf7dce
Gustvavo de oliveira <gustvavodeoliveira@Gustavodeoliveira>
2024-01-09 22:28:58 -0300

    Feat: communication with api complete

--- front-end/src/hook/Sign.tsx
+++ front-end/src/hook/Sign.tsx
@@ -1,15 +1,45 @@
 import React, { useRef } from 'react';
 import { useForm } from 'react-hook-form';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
-const Sign = (): JSX.Element => {
+const Sign = () => {
 	const { register, handleSubmit } = useForm();
+	const name = useRef<HTMLInputElement | null>(null);
+
 	const email = useRef<HTMLInputElement | null>(null);
 	const password = useRef<HTMLInputElement | null>(null);
+	const ConfirmPassword = useRef<HTMLInputElement | null>(null);
 	const { ref, ...rest } = register('email');
+	const { ref: NameRef, ...Namerest } = register('name');
 	const { ref: PasswordRef, ...pasrest } = register('password');
+	const { ref: ConfirmPasswordRef, ...ConfirmPassrest } =
+		register('ConfirmPassword');
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const onSubmit = (data) => {
+		axios
+			.post('https://blog-a34y.onrender.com/user/create', data, {
+				headers: {
+					'Content-Type': 'application/json',
+					// 'Content-Type': 'application/x-www-form-urlencoded',
+				},
+			})
+			.then((res: AxiosResponse) => console.log(res))
+			.catch((e: AxiosError) => console.log(e));
+	};
 
 	return (
-		<form onSubmit={handleSubmit((data) => console.log(data))}>
+		<form onSubmit={handleSubmit(onSubmit)} method="post">
+			<input
+				{...Namerest}
+				name="name"
+				type="text"
+				placeholder="E-mail"
+				ref={(e) => {
+					NameRef(e);
+					email.current = e; // you can still assign to ref
+				}}
+			/>
 			<input
 				{...rest}
 				name="email"
@@ -28,6 +58,16 @@ const Sign = (): JSX.Element => {
 				ref={(e) => {
 					PasswordRef(e);
 					password.current = e; // you can still assign to ref
+				}}
+			/>
+			<input
+				{...ConfirmPassrest}
+				name="ConfirmPassword"
+				type="password"
+				placeholder="Password"
+				ref={(e) => {
+					ConfirmPasswordRef(e);
+					ConfirmPassword.current = e; // you can still assign to ref
 				}}
 			/>
 			<button type="submit">Submit</button>
