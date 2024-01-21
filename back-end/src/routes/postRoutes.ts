f112da36f6e067a240cef17d441b6571bba6d01d
Gustvavo de oliveira <gustvavodeoliveira@Gustavodeoliveira>
2024-01-21 10:22:26 -0300

    feat: create route one post

--- back-end/src/routes/postRoutes.ts
+++ back-end/src/routes/postRoutes.ts
@@ -10,6 +10,7 @@ const upload = multer(ConfigUploadImage);
 
 postRouter.get('/allpost', checkToken, PostController.getAllPost);
 postRouter.get('/posts/:categoric', checkToken, PostController.getPostCategoric);
+postRouter.get('/:id', checkToken, PostController.getPost);
 postRouter.get('/myposts', checkToken, PostController.getMyPost);
 postRouter.post('/posted', checkToken,upload.array('image[]', 3),PostController.postedPost);
 postRouter.patch('/edit/:postId', checkToken, upload.array('image'),PostController.PostUpdate);
